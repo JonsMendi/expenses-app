dcd4091fb8f85e3c5ca422098a2aa1196da4b7b4
JonsMendi <93676149+JonsMendi@users.noreply.github.com>
2022-06-21 22:12:56 +0200

    Created new expenses filter to filter the expenses by year.
    If applied, this commit will have created new expenses filter to filter the expenses by year.

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,26 +1,45 @@
+import React, { useState } from "react";
 import Card from "../UI/Card";
 import ExpenseItem from "./ExpenseItem";
+import ExpensesFilter from "./ExpensesFilter";
 import "./Expenses.css";
 
 const Expenses = (props) => {
+  const [filteredYear, setFilteredYear] = useState("2022");
+
+  // Under is where the year(value) selected in ExpensesFilter.js will be saved and the set in the state.
+  // This happen putting the value of selectedDateHandler function in ExpensesFilter.js in setFilteredYear.
+  const savedSelectedDate = (selectedYear) => {
+    setFilteredYear(selectedYear);
+    console.log(selectedYear);
+  };
+
   return (
     <Card className="expenses">
+      <ExpensesFilter
+        selected={filteredYear}
+        onSaveSelectedDate={savedSelectedDate}
+      />
       <ExpenseItem
+        //Parent-Child-Props
         title={props.expenses[0].title}
         amount={props.expenses[0].amount}
         date={props.expenses[0].date}
       />
       <ExpenseItem
+        //Parent-Child-Props
         title={props.expenses[1].title}
         amount={props.expenses[1].amount}
         date={props.expenses[1].date}
       />
       <ExpenseItem
+        //Parent-Child-Props
         title={props.expenses[2].title}
         amount={props.expenses[2].amount}
         date={props.expenses[2].date}
       />
       <ExpenseItem
+        //Parent-Child-Props
         title={props.expenses[3].title}
         amount={props.expenses[3].amount}
         date={props.expenses[3].date}
---
+++ src/components/Expenses/ExpensesFilter.js
@@ -0,0 +1,29 @@
+import React from "react";
+import Card from "../UI/Card";
+import "./ExpensesFilter.css";
+
+// This component is the responsible to listen for a change of selected year.
+// After selected a year. This data will be saved in selectedDateHandler function through onChange.
+// Inside of selectedDateHandler function we will save the selected year in onSaveSelectedDate function that is defined on Expenses.js
+// Like this, the year(value) will Lift the State Up to the parent component and there, the state will be defined
+
+const ExpensesFilter = (props) => {
+  const selectDateHandler = (event) => {
+    props.onSaveSelectedDate(event.target.value);
+  };
+
+  return (
+    <Card className="expenses-filter">
+      <div className="expenses-filter__control">
+        <label>Filter by year</label>
+        <select value={props.selected} onChange={selectDateHandler}>
+          <option value="2022">2022</option>
+          <option value="2021">2021</option>
+          <option value="2020">2020</option>
+        </select>
+      </div>
+    </Card>
+  );
+};
+
+export default ExpensesFilter;
